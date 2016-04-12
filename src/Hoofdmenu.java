9de5e25f2e921736cd853ea6c2990ab563903a0d
stefanschmeltz1991 <0904181@hr.nl>
2016-04-12 10:41:08 +0200

    stylesheet, background.update

--- src/Hoofdmenu.java
+++ src/Hoofdmenu.java
@@ -3,6 +3,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +15,15 @@ public class Hoofdmenu extends Application{
     @Override public void start(Stage showedStage) throws Exception {
         stage = showedStage;
         showedStage.setTitle("");
+
+        stage = showedStage;
+        showedStage.setTitle("bycicle");
+
+        //text
+        Text text = new Text("welcome to SaveByc.");
+        text.setTranslateX(200);
+        text.setTranslateY(200);
+        text.setId("fancytext");
 
         CheckBox box1   = new CheckBox(BuurtInfo.Charlois.getNaam());
         CheckBox box2   = new CheckBox(BuurtInfo.Delfshaven.getNaam());
@@ -30,27 +40,64 @@ public class Hoofdmenu extends Application{
         CheckBox box13  = new CheckBox(BuurtInfo.RotterdamCentrum.getNaam());
         CheckBox box14  = new CheckBox(BuurtInfo.Rozenburg.getNaam());
 
+        box1.setTranslateX(50);
+        box2.setTranslateX(50);
+        box3.setTranslateX(50);
+
+        box4.setTranslateX(300);
+        box5.setTranslateX(300);
+        box6.setTranslateX(300);
+
+        box7.setTranslateX(600);
+        box8.setTranslateX(600);
+        box9.setTranslateX(600);
+
+        box10.setTranslateX(900);
+        box11.setTranslateX(900);
+        box12.setTranslateX(900);
+
+        //Y
+        box1.setTranslateY(240);
+        box2.setTranslateY(240);
+        box3.setTranslateY(240);
+
+        box4.setTranslateY(100);
+        box5.setTranslateY(100);
+        box6.setTranslateY(100);
+
+        box7.setTranslateY(-45);
+        box8.setTranslateY(-45);
+        box9.setTranslateY(-45);
+
+        box10.setTranslateY(-190);
+        box11.setTranslateY(-190);
+        box12.setTranslateY(-190);
 
 
         box1.setSelected(true);
         box2.setSelected(true);
         box3.setSelected(true);
-        box1.setTranslateX(200);
-        box1.setTranslateY(400);
 
-        Button grafiekButton = new Button("Klik hier");
+
+        Button grafiekButton = new Button("show chart");
+        grafiekButton.setTranslateX(500);
+        grafiekButton.setTranslateY(200);
+
         grafiekButton.setOnAction(e -> {
             checkboxHandler(box1, box2, box3, box4, box5, box6, box7, box8, box9, box10, box11, box12, box13, box14);
             Grafiek.display();
         });
 
         VBox testLayout = new VBox(20);
-        testLayout.getChildren().addAll(grafiekButton, box1, box2, box3, box4, box5, box6, box7, box8, box9, box10, box11, box12, box13, box14);
+        testLayout.getChildren().addAll(text,grafiekButton, box1, box2, box3, box4, box5, box6, box7, box8, box9, box10, box11, box12, box13, box14);
 
         Scene mainScene = new Scene(testLayout, 1200,700);
 
         showedStage.setScene(mainScene);
+
+        mainScene.getStylesheets().add("Stylesheet.css");
         showedStage.show();
+
     }
 
     public static void checkboxHandler(CheckBox box1, CheckBox box2, CheckBox box3, CheckBox box4, CheckBox box5, CheckBox box6, CheckBox box7, CheckBox box8, CheckBox box9,CheckBox box10, CheckBox box11, CheckBox box12, CheckBox box13, CheckBox box14){
